a62ea2183ebbb59b722abc6133927dc9014781df
Giorgio Andrei <cipuandrei@libero.it>
2021-09-09 15:54:38 +0200

    header-created

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Header />
+  </div>
 </template>
 
 <script>
-/* import HelloWorld from './components/HelloWorld.vue' */
+import Header from "./components/Header.vue";
 
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+  },
 };
 </script>
 
 <style lang="scss">
+@import "./assets/scss/_style.scss";
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <div class="container">
+      <img src="" alt="" />
+      <select>
+        <option value="All" selected>All</option>
+        <option></option>
+      </select>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+/* @import "/scss/_vars.scss"; */
+header {
+  /*   background-color: $dark-blue; */
+  height: 70px;
+  padding: 10px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+}
+
+header > div.container {
+  display: flex;
+  justify-content: space-between;
+}
+
+header > div.container select {
+  height: 20px;
+  align-self: flex-end;
+}
+
+header img {
+  height: 50px;
+}
+</style>
